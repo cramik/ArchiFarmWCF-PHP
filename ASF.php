8c75ad3faf5cee046f6b62ab75883198d664d560
cramik <5573629+cramik@users.noreply.github.com>
2017-11-07 12:54:16 -0800

    Update ASF.php

--- ASF.php
+++ ASF.php
@@ -1,8 +1,7 @@
-<?php include("C:\\xampp\\htdocs\\password_protect.php"); ?>
 <?php
 if (isset($_GET['cmd'])) {
 	$cmd = $_GET['cmd'];
-	$output = exec("C:\Users\Cramik\Desktop\ASF\ASF.exe --client" . " " . $cmd);
+	$output = exec("*INSERT ASF DIRECTORY*/ASF.exe --client" . " " . $cmd);
 	print_r($output);
 }else{
     echo "No Command Given";
